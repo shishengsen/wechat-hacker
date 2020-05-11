44e4c244652ccb6694e5b0a9c18c49f9fdf53565
Fangtian <fangtian@medlinker.com>
2020-05-11 10:16:08 +0800

    make proto shell

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-protoc --proto_path=../XMagicHooker-Cmd --go_out=proto ../XMagicHooker-Cmd/Cmd.proto ../XMagicHooker-Cmd/WwCmd.proto ../XMagicHooker-Cmd/WxCmd.proto
+cd ../XMagicHooker-Cmd && git pull && cd ../wechat-hacker && protoc --proto_path=../XMagicHooker-Cmd --go_out=proto ../XMagicHooker-Cmd/Cmd.proto ../XMagicHooker-Cmd/WwCmd.proto ../XMagicHooker-Cmd/WxCmd.proto
